36686522499a624683aa7b4d80e7377e847c683e
humayonitbd <humayonforid44@gmail.com>
2025-05-20 15:20:07 +0600

    update code push

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -9,30 +9,13 @@ const paymentRouter = express.Router();
 
 paymentRouter
   .post('/add-payment', auth(USER_ROLE.USER), paymentController.addPayment)
-  //   .post(
-  //     '/create-stripe-account',
-  //     auth(USER_ROLE.BUSINESS),
-  //     paymentController.createStripeAccount,
-  //   )
-  //   .post(
-  //     '/transfer',
-  //     auth(USER_ROLE.BUSINESS),
-  //     paymentController.transferBalance,
-  //   )
 
-  //   .post(
-  //   '/checkout',
-  //   auth(USER_ROLE.CUSTOMER),
-  //   paymentController.createCheckout,
-  // )
-  //   .post('/refund', paymentController.paymentRefund)
   .get('/success', paymentController.successPage)
   .get('/cancel', paymentController.cancelPage)
 
   .get('/', 
     // auth(USER_ROLE.ADMIN), 
     paymentController.getAllPayment)
-  // .get('/payment-tracking', auth(USER_ROLE.CUSTOMER), paymentController.getAllPaymentByCustomer)
   .get('/all-income-rasio', paymentController.getAllIncomeRasio)
   .get('/all-income-rasio-by-days', paymentController.getAllIncomeRasioBydays)
   .get(
@@ -41,9 +24,7 @@ paymentRouter
     paymentController.getAllEarningRasio,
   )
 
-  //   .get('/refreshAccountConnect/:id', paymentController.refreshAccountConnect)
   .get('/:id', paymentController.getSinglePayment)
-  //   .get('/success-account/:id', paymentController.successPageAccount)
 
   .delete('/:id', paymentController.deleteSinglePayment);
 
